4f7af761be2db388e9738fcf1151a9ed5f7dc303
sabiya-rashid <bhatsabiya2@gmail.com>
2023-09-19 11:23:55 +0530

    Add project files.

---
+++ Models/Base/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Base
+{
+    public class BaseModel
+    {
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime? UpdatedDate { get; set; }        
+    }
+}
