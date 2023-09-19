6d41d0982de03e2ea9b811e4fbabb34b3d8d5c09
sabiya-rashid <bhatsabiya2@gmail.com>
2023-09-19 12:01:55 +0530

    Models updated

--- Models/Property.cs
+++ Models/Property.cs
@@ -9,5 +9,19 @@ namespace Models
 {
     public class Property: BaseModel
     {
-    }
+        public int Id { get; set; } 
+        public string Type { get; set; }
+        public string Description { get; set;}
+        public string Address { get; set; }
+        public int Size { get; set; }
+        public int BedroomsCount { get; set; }
+        public int BathroomsCount { get; set; }
+        public float Price { get; set; }
+        public string Status { get; set; }
+        public string Features { get; set;}
+        public int UserId { get; set;}
+        public User User { get; set; }
+        public bool Verrified { get; set; } = false;
+        public ICollection<Contract>? Contracts { get; set; }
+    }    
 }
